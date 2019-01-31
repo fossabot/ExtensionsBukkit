7a374258ee218fe5e2345ce2d1a5efc9269af58c
TSEngineer <pamnlyr@gmail.com>
2019-01-31 20:47:38 +0300

    Clean up imports

--- src/main/java/net/extbukkit/api/loader/IExtensionLoader.java
+++ src/main/java/net/extbukkit/api/loader/IExtensionLoader.java
@@ -1,8 +1,5 @@
 package net.extbukkit.api.loader;
 
-import net.extbukkit.api.extension.Extension;
-import net.extbukkit.api.log.ILogger;
-
 import java.io.File;
 
 public interface IExtensionLoader {
--- src/main/java/net/extbukkit/main/BukkitExtensionsBukkit.java
+++ src/main/java/net/extbukkit/main/BukkitExtensionsBukkit.java
@@ -1,15 +1,11 @@
 package net.extbukkit.main;
 
-import com.google.common.hash.Hashing;
-import com.google.common.io.Files;
 import net.extbukkit.api.builtin.events.EventLoad;
 import net.extbukkit.main.server.Server;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
-import java.io.IOException;
-import java.security.MessageDigest;
 
 public final class BukkitExtensionsBukkit extends JavaPlugin {
     private static BukkitExtensionsBukkit I;
