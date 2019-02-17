841cf60280ec25dcdb8bc4c4d05c62fd84f7ab0a
TSEngineer <pamnlyr@gmail.com>
2019-02-17 15:58:07 +0300

    Things

--- src/main/java/ml/extbukkit/api/event/Handler.java
+++ src/main/java/ml/extbukkit/api/event/Handler.java
@@ -1,5 +1,10 @@
 package ml.extbukkit.api.event;
 
+import java.lang.annotation.*;
+
+@Documented
+@Target(value = ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
 public @interface Handler {
-    HandlePriority priority();
+    HandlePriority priority() default HandlePriority.NORMAL;
 }
--- src/main/java/ml/extbukkit/main/manager/EventManager.java
+++ src/main/java/ml/extbukkit/main/manager/EventManager.java
@@ -1,6 +1,5 @@
 package ml.extbukkit.main.manager;
 
-import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.ListMultimap;
 import com.google.common.collect.Multimaps;
 import ml.extbukkit.api.event.Event;
@@ -12,21 +11,21 @@ import java.lang.annotation.Annotation;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.IdentityHashMap;
+import java.util.Set;
 
 public class EventManager implements IEventManager {
     private ListMultimap<IHandlerContainer, Method> mtdl = Multimaps.synchronizedListMultimap(Multimaps.newListMultimap(new IdentityHashMap<>(), ArrayList::new));
 
     @Override
     public void callEvent(Event event) {
-        System.out.println("CALLEVENT!");
         for (IHandlerContainer c : mtdl.keySet()) {
             for (Method m : mtdl.get(c)) {
                 if (event.getClass().isAssignableFrom(m.getParameterTypes()[0])) {
                     m.setAccessible(true);
                     try {
                         m.invoke(c, event);
-                        System.out.println("INVOKE!");
                     } catch (IllegalAccessException | InvocationTargetException e) {
                         e.printStackTrace();
                     }
@@ -37,20 +36,10 @@ public class EventManager implements IEventManager {
 
     @Override
     public void registerContainer(IHandlerContainer container) {
-        System.out.println("Registering container");
         registerContainerMethods(container);
     }
 
     public void registerContainerMethods(IHandlerContainer c) {
-        Class<? extends IHandlerContainer> hc = c.getClass();
-        Method[] mtd = hc.getMethods();
-        for (Method m : mtd) {
-            Annotation a = m.getAnnotation(Handler.class);
-            if (a == null)
-                continue;
-            if (m.getParameterCount() == 1 && m.getReturnType() == void.class && Event.class.isAssignableFrom(m.getParameterTypes()[0]))
-                mtdl.put(c, m);
-            System.out.println(m.getName());
-        }
+
     }
 }
