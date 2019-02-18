5d031816fa3af14a588bcf83e996a2d878dce1a7
MrIvanPlays <pekov.ivan@abv.bg>
2019-02-18 16:41:39 +0200

    Fixes and comments. I give up on event calling...

--- src/main/java/ml/extbukkit/main/secure/bukkit/BukkitExtensionsBukkit.java
+++ src/main/java/ml/extbukkit/main/secure/bukkit/BukkitExtensionsBukkit.java
@@ -3,6 +3,11 @@ package ml.extbukkit.main.secure.bukkit;
 import ml.extbukkit.api.builtin.events.EventLoad;
 import ml.extbukkit.main.secure.command.CommandManager;
 import ml.extbukkit.main.secure.server.Server;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.LogEvent;
+import org.apache.logging.log4j.core.Logger;
+import org.apache.logging.log4j.core.filter.AbstractFilter;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
@@ -21,15 +26,15 @@ public final class BukkitExtensionsBukkit extends JavaPlugin {
     @Override
     public void onLoad() {
         Server.getInstance();
-        /*((Logger) LogManager.getRootLogger()).addFilter( new AbstractFilter()
-        {
+        ((Logger) LogManager.getRootLogger()).addFilter( new AbstractFilter() {
             @Override
-            public Result filter(LogEvent event)
-            {
-                ((ml.extbukkit.main.secure.log.Logger) Server.getInstance().getLogger()).logBukkit( event.getLevel(), event.getMessage().getFormattedMessage() );
+            public Result filter(LogEvent event) {
+                ((ml.extbukkit.main.secure.log.Logger) Server.getInstance().getLogger()).logBukkit(event.getLevel(), event.getMessage().getFormattedMessage());
+                if(event.getThrown() != null)
+                    ((ml.extbukkit.main.secure.log.Logger) Server.getInstance().getLogger()).logBukkit(event.getLevel(), ExceptionUtils.getStackTrace(event.getThrown()));
                 return Result.DENY;
             }
-        } );*/
+        });
         if (!getServer().getVersion().contains("1.13")) {
             Server.getInstance().getLogger().log("Unsupported Minecraft version found, we should disable server. Only minecraft 1.13 is supported!");
             getServer().shutdown();
